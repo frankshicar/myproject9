dc7ad06df042ab86c5e00e2d46564365e7bf0d28
frankshicar <93965977+frankshicar@users.noreply.github.com>
2022-06-10 15:06:44 +0800

    initial commit

---
+++ src/test/java/com/frank/Hello.java
@@ -0,0 +1,9 @@
+package com.frank;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Person p = new Person();
+        p.gretting();
+    }
+}
---
+++ src/test/java/com/frank/Person.java
@@ -0,0 +1,7 @@
+package com.frank;
+
+public class Person {
+    public void gretting(){
+        System.out.println("Hello!");
+    }
+}
