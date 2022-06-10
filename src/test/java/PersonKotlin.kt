7c9cf4923519da18e6e1153dd159a17a8cc882cf
frankshicar <93965977+frankshicar@users.noreply.github.com>
2022-06-10 16:33:15 +0800

    initial commit

---
+++ src/test/java/PersonKotlin.kt
@@ -0,0 +1,13 @@
+package com.frank
+
+class PersonKotlin {
+    fun greeting() {
+        println("hello kotlin")
+    }
+}
+
+//Top-level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
