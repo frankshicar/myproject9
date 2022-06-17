6f05fd2e34d0576d55b69ec20b4b4b44ff82f1df
frankshicar <93965977+frankshicar@users.noreply.github.com>
2022-06-17 15:17:20 +0800

    initial commit

---
+++ src/test/java/score/Student.kt
@@ -0,0 +1,17 @@
+package com.frank.score
+
+class Student (val name: String, var english:Int, var math:Int){
+    init{
+//        println(:Testing);
+    }
+    fun print(){
+        println("$name\t$english\t$math\t${getAverage()}")
+    }
+    fun getAverage() : Int{
+        return (english+math)/2
+    }
+}
+fun main(){
+    val stu = Student("Jack",55,87);
+    stu.print();
+}
